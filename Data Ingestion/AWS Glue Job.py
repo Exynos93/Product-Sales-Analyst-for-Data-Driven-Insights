d0be1d3f466813dad6ea71b886c817aaef5d592f
Qowiyu Yusrizal <109859143+Exynos93@users.noreply.github.com>
2024-12-13 15:18:23 +0700

    AWS Glue Job.py

---
+++ Data Ingestion/AWS Glue Job.py
@@ -0,0 +1,17 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+# Here, you'd replicate the ETL steps from main_etl_pipeline.py using Glue's methods
+
+job.commit()
